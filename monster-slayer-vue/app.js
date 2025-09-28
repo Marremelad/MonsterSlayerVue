498f217a18d0675a8cf7fcda4100054e45e04325
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-09-28 15:29:34 +0200

    (feature) improved the way healthbars are displayed and added special attack

--- monster-slayer-vue/app.js
+++ monster-slayer-vue/app.js
@@ -1,28 +1,47 @@
-function getAttackValue(min, max) {
+function getRandomValue(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
 Vue.createApp({
   data() {
     return {
+      playerMaxHealth: 100,
       playerHealth: 100,
-      monsterHealth: 100,
+      monsterMaxHealth: 150,
+      monsterHealth: 150,
     };
   },
   methods: {
-    resolveCombat() {
-      this.monsterHealth -= getAttackValue(5, 12);
-      this.playerHealth -= getAttackValue(8, 15);
+    resolveCombat(playerMinDmg, playerMaxDmg) {
+      console.log(this.playerHealth);
+      this.monsterHealth = Math.max(
+        0,
+        this.monsterHealth - getRandomValue(playerMinDmg, playerMaxDmg)
+      );
+
+      this.playerHealth = Math.max(
+        0,
+        this.playerHealth - getRandomValue(8, 15)
+      );
     },
-    attackMonster() {
-      const attackValue = getAttackValue(5, 12);
-      this.monsterHealth -= attackValue;
-    },
-    attackPlayer() {
-      const attackValue = getAttackValue(8, 15);
-      this.playerHealth -= attackValue;
+    healPlayer() {
+      this.playerHealth = Math.min(
+        this.playerMaxHealth,
+        this.playerHealth + getRandomValue(10, 20)
+      );
     },
   },
-  computed: {},
+  computed: {
+    monsterHealthBar() {
+      return (
+        Math.max(0, (this.monsterHealth / this.monsterMaxHealth) * 100) + "%"
+      );
+    },
+    playerHealthBar() {
+      return (
+        Math.max(0, (this.playerHealth / this.playerMaxHealth) * 100) + "%"
+      );
+    },
+  },
   watch: {},
 }).mount("#game");
